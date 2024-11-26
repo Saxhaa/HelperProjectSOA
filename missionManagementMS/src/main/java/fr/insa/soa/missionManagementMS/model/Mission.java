61edb1c62f2f7cf128fc089ab0f2897b87f617da
Sacha <sacha.bonicatto@gmail.com>
2024-11-26 14:24:29 +0100

    micro service mission et discovery

---
+++ missionManagementMS/src/main/java/fr/insa/soa/missionManagementMS/model/Mission.java
@@ -0,0 +1,64 @@
+package fr.insa.soa.missionManagementMS.model;
+
+
+public class Mission {
+    private int missionId;
+    private String name;
+    private String description;
+
+    private int personInNeedId; // ID du demandeur
+    private int helperId; // ID du volontaire (par défaut 0)
+    private int status; // 0: en attente, 1: en cours, 2: acceptée, 3: refusée
+    
+    public Mission() {
+    	
+    }
+    // Getters et setters
+    public int getMissionId() {
+        return missionId;
+    }
+
+    public void setMissionId(int missionId) {
+        this.missionId = missionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPersonInNeedId() {
+        return personInNeedId;
+    }
+
+    public void setPersonInNeedId(int personInNeedId) {
+        this.personInNeedId = personInNeedId;
+    }
+
+    public int getHelperId() {
+        return helperId;
+    }
+
+    public void setHelperId(int helperId) {
+        this.helperId = helperId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
---
+++ missionManagementMS/src/test/java/fr/insa/soa/missionManagementMS/MissionManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.soa.missionManagementMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionManagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
