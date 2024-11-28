eb3a6f88111dc8938c6bc7540ffc8dd65308a91e
Binary-Human <mll.sara.bakri@gmail.com>
2024-11-28 18:35:45 +0100

    working on configuration microservice side - Configuration service has been setup

--- mission-service/src/main/java/fr/insa/soa/missionService/MissionServiceApplication.java
+++ mission-service/src/main/java/fr/insa/soa/missionService/MissionServiceApplication.java
@@ -4,12 +4,15 @@ import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 
 public class MissionServiceApplication {
 
 	public static void main(String[] args) {
+
+		ConfigurableApplicationContext context = SpringApplication.run(MissionServiceApplication.class, args);
 		SpringApplication.run(MissionServiceApplication.class, args);
 	}
 
