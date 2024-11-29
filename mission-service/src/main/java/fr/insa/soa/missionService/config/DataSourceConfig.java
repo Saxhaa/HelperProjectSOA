e141f149a94f22f217d935957c6c5001785393a2
Binary-Human <mll.sara.bakri@gmail.com>
2024-11-29 13:22:15 +0100

    trying with just missionService

---
+++ mission-service/src/main/java/fr/insa/soa/missionService/config/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package fr.insa.soa.missionService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Autowired
+    private DatabaseProperties databaseProperties;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url(databaseProperties.getUrl())
+                .username(databaseProperties.getUsername())
+                .password(databaseProperties.getPassword())
+                .driverClassName(databaseProperties.getDriverClassName())
+                .build();
+    }
+}
+
+
