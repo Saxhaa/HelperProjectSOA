1ec3e828d67ee533ad4b77329579d0ddf835b8ca
Binary-Human <mll.sara.bakri@gmail.com>
2024-11-29 13:46:20 +0100

    configuration for missionService works (FINALLLLLLLLY)

--- mission-service/src/main/java/fr/insa/soa/missionService/MissionServiceApplication.java
+++ mission-service/src/main/java/fr/insa/soa/missionService/MissionServiceApplication.java
@@ -1,16 +1,11 @@
 package fr.insa.soa.missionService;
 
-import com.netflix.discovery.EurekaNamespace;
-import fr.insa.soa.missionService.config.DataSourceConfig;
-import fr.insa.soa.missionService.config.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@EnableConfigurationProperties(DatabaseProperties.class)
 public class MissionServiceApplication {
 
 	public static void main(String[] args) {
